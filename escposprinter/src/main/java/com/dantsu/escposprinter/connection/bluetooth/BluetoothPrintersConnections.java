c6a37695b29cb376ee4c0570d7cacbb7b178c35b
Luis Solorzano <luisnic20@gmail.com>
2023-09-25 21:40:35 -0600

    Fixed imaging

--- escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothPrintersConnections.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothPrintersConnections.java
@@ -50,15 +50,23 @@ public class BluetoothPrintersConnections extends BluetoothConnections {
       return null;
     }
 
-        int i = 0;
-        BluetoothConnection[] printersTmp = new BluetoothConnection[bluetoothDevicesList.length];
-        for (BluetoothConnection bluetoothConnection : bluetoothDevicesList) {
-            BluetoothDevice device = bluetoothConnection.getDevice();
-            printersTmp[i++] = new BluetoothConnection(device);
-        }
-        BluetoothConnection[] bluetoothPrinters = new BluetoothConnection[i];
-        System.arraycopy(printersTmp, 0, bluetoothPrinters, 0, i);
-        return bluetoothPrinters;
+    int i = 0;
+    BluetoothConnection[] printersTmp = new BluetoothConnection[bluetoothDevicesList.length];
+    for (BluetoothConnection bluetoothConnection : bluetoothDevicesList) {
+      BluetoothDevice device = bluetoothConnection.getDevice();
+
+      int majDeviceCl = device.getBluetoothClass().getMajorDeviceClass(),
+        deviceCl = device.getBluetoothClass().getDeviceClass();
+
+      if (majDeviceCl == BluetoothClass.Device.Major.IMAGING && (deviceCl == 1664 || deviceCl == BluetoothClass.Device.Major.IMAGING)) {
+        printersTmp[i++] = new BluetoothConnection(device);
+      } else if (majDeviceCl == BluetoothClass.Device.Major.UNCATEGORIZED && (deviceCl == BluetoothClass.Device.Major.UNCATEGORIZED)) {
+        printersTmp[i++] = new BluetoothConnection(device);
+      }
     }
+    BluetoothConnection[] bluetoothPrinters = new BluetoothConnection[i];
+    System.arraycopy(printersTmp, 0, bluetoothPrinters, 0, i);
+    return bluetoothPrinters;
+  }
 
 }
